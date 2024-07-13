c59cce9db7bc4bfdc113b7c10e6577ed7462b5cb
Joshua Ofosu <joeokat@gmail.com>
2024-07-13 19:32:45 +0000

    init

---
+++ kidhacker/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a starter template for Next.js projects.</p>
+      </main>
+    </div>
+  );
+}
